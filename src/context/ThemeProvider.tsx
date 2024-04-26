a172080a72320831832effa5a9e1165be9d37b31
Mohanad <info@mohnd-info.com>
2024-04-26 20:33:32 +0300

    setup theme

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState<string>("");
+
+  const handleThemeChange = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    } else {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+
+  return context;
+}
